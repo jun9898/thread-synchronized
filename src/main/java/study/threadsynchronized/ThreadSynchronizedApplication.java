ba3d71a902199067a6710f1558eb87f783fe43e8
jun9898 <nice1998@gmail.com>
2024-08-31 14:00:45 +0900

    project init

---
+++ src/main/java/study/threadsynchronized/ThreadSynchronizedApplication.java
@@ -0,0 +1,13 @@
+package study.threadsynchronized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadSynchronizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadSynchronizedApplication.class, args);
+	}
+
+}
